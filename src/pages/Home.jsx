19ef423fd16cd1da3cc9556e13dfd22ee7d8dde3
David Vasquez <dvasquez.422@gmail.com>
2025-03-23 13:36:10 -0600

    Added boilerplate code for placeholders

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import Nav from "../components/Nav";
 
 const HomePage = () => {
   return (
     <div>
+      <Nav />
       <h1 className="text-2xl">Homepage</h1>
     </div>
   );
