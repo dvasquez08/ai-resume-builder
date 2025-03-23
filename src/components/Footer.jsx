82b547e909194ba22b0b4a2cb4d4dba054d416f8
David Vasquez <dvasquez.422@gmail.com>
2025-03-23 14:10:56 -0600

    Completed Footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,25 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <>
+      <footer className="rounded-lg shadow m-4 bg-sky-900">
+        <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+          <span className="text-sm sm:text-center text-gray-400">
+            © 2025{" "}
+            <a
+              href="https://dvasquez.net"
+              target="_blank"
+              className="hover:underline"
+            >
+              David Vasquez{" "}
+            </a>
+            . All Rights Reserved.
+          </span>
+        </div>
+      </footer>
+    </>
+  );
 };
 
 export default Footer;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Nav from "../components/Nav";
+import Footer from "../components/Footer";
 
 const HomePage = () => {
   return (
     <div>
       <Nav />
       <h1 className="text-2xl">Homepage</h1>
+      <Footer />
     </div>
   );
 };
